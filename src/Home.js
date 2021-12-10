da23559be7a4d99716e5763a976b3e9654010265
john-scripla <80074354+john-scripla@users.noreply.github.com>
2021-12-10 11:13:11 -0500

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Feed from './Feed'
+import Feed from './Feed';
+import { useContext } from 'react';
+import DataContext from './context/DataContext';
 
- const Home = ({posts}) => {
+const Home = () => {
+    const { searchResults, fetchError, isLoading } = useContext(DataContext);
+
     return (
         <main className="Home">
-            {posts.length ? (
-                <Feed posts={posts} /> 
-            ) : (
-                <p>No posts to display</p>
-            
-            )}
+            {isLoading && <p className="statusMsg">Loading posts...</p>}
+            {!isLoading && fetchError && <p className="statusMsg" style={{ color: "red" }}>{fetchError}</p>}
+            {!isLoading && !fetchError && (searchResults.length ? <Feed posts={searchResults} /> : <p className="statusMsg">No posts to display.</p>)}
         </main>
     )
 }
 
-export default Home;
+export default Home
